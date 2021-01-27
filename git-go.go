4e1b03b6737b33ce8a18442e21f0835bb4004508
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-01-27 12:33:56 -0800

    Update staticcheck version.

--- git-go.go
+++ git-go.go
@@ -238,7 +238,7 @@ git go %s
 
 func installTools() error {
 	for _, tool := range []string{
-		"honnef.co/go/tools/cmd/staticcheck@2020.2",
+		"honnef.co/go/tools/cmd/staticcheck@2020.2.1",
 	} {
 		cmd := exec.Command("go", "get", "-u", tool)
 		cmd.Dir = os.TempDir()
